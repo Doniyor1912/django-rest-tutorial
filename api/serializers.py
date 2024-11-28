1b5cec13c5999d83c5055f3f4557838768fdbdbe
Doniyor <domanick191201@gmail.com>
2024-11-28 18:08:38 +0500

    project

--- api/serializers.py
+++ api/serializers.py
@@ -4,7 +4,6 @@ from api.models import Container
 
 
 class SerializerPerson(serializers.ModelSerializer):
-    owner = serializers.HiddenField(default=serializers.CurrentUserDefault)
     class Meta:
         model = Container
         fields = "__all__"
--- api/views.py
+++ api/views.py
@@ -3,10 +3,7 @@ from typing import _generic_init_subclass
 from django.shortcuts import render
 from drf_spectacular.utils import extend_schema_view, extend_schema
 from rest_framework import viewsets, mixins, generics
-from rest_framework.viewsets import GenericViewSet
-from rest_framework import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from api.models import Container
 from api.serializers import SerializerPerson
 
@@ -30,13 +27,6 @@ class Person(mixins.CreateModelMixin,
 
 #----------------------------
 
-#generics-----------------------
-class Unity(generics.ListAPIView):
-    queryset = Person.objects.all()
-    serializer_class = SerializerPerson
-    permission_classes = (IsAuthenticatedOrReadOnly,)
-
-#-------------------------
 
 #get-all --> malumotlar kelishini cheklash uchun------
     def get_queryset(self, pk=None,*args, **kwargs):
@@ -46,95 +36,3 @@ class Unity(generics.ListAPIView):
         return Container.objects.filter(pk=pk)
 
 #-----------------------------------
-
-
-
-#action-------
-    @action(methods=['get'],detail=True)
-    def category(self,request, pk=None):
-        pk = self.kwargs.get("pk")
-        if not pk:
-            return Person.objects.all()
-        return Person.objects.filter(pk=pk)
-#-----------------
-
-
-class PersonList(APIView):
-
-    def get(self,request, *args, **kwargs):
-        pk = self.kwargs.get("pk",None)
-        if pk:
-            if not pk:
-                return Response({"Answer":"Get not allowed"})
-            try:
-                instance = Person.objects.get(pk=pk)
-
-            except:
-                return Response({"Answer": "Data not found"})
-
-            serializer = SerializerPerson(instance)
-            return Response({"Person":serializer.data})
-
-        else:
-            list_person = Person.objects.all()
-            return Response({"Person":SerializerPerson(list_person, many=True).data})
-
-    def post(self,request):
-        serializer = SerializerPerson(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({"Person":serializer.data})
-
-    def put(self, request, *args, **kwargs):
-        pk = kwargs.get("pk", None)
-        if not pk:
-            return Response({"Answer": "Method Put not allowed!"})
-        try:
-            intance = Person.objects.get(pk=pk)
-
-        except:
-            return Response({"Answer": "DAta not found!"})
-
-        serializer = SerializerPerson(data=request.data, instance=intance)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({"Person":serializer.data})
-
-
-    def pacht(self, request, *args, **kwargs):
-        pk = kwargs.get("pk",None)
-        if not pk:
-            return Response({"Answer": "Method pacht not allowed!"})
-        try:
-            instance = Person.objects.get(pk=pk)
-
-        except:
-            return Response({"Answer": " Data not found!"})
-
-        serializer = SerializerPerson(data=request.data, instance=instance, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({"Person": serializer.data})
-
-
-
-    def delete(self, *args, **kwargs):
-        pk = kwargs.get("pk", None)
-        if not pk:
-            return Response({"Answer": "Method Delete not allowed!"})
-
-        try:
-            instance = Person.objects.get(pk=pk)
-            instance.delete()
-        except:
-            return Response({"Answer": "Data not found!"})
-
-        return Response({"Person":f"{pk}-id -> deleted"})
-
-
-
-
-
-
-
-
